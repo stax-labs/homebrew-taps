4da6ca2aa79aa7339d2c0fd98e00565a8410114c
Josh Wong <josh@stax.io>
2021-01-28 12:14:52 +1100

    chore(platform): stax2aws release v1.2.2

--- stax2aws.rb
+++ stax2aws.rb
@@ -2,13 +2,13 @@ require 'formula'
 
 class Stax2aws < Formula
   homepage 'https://github.com/stax-labs/stax2aws-releases'
-  version '1.2.1'
+  version '1.2.2'
   if OS.mac?
     url "https://github.com/stax-labs/stax2aws-releases/releases/download/v#{version}/stax2aws_#{version}_macOS_amd64.tar.gz"
-    sha256 '54a6393d9b82e9e680031191b00e64d4112ea81250e76c298ee4571786d00b40'
+    sha256 '7a46d6bb68285d7ba44809bed9688d34b9a79e817f98df347ddfa64795b7ecc9'
   elsif OS.linux?
     url "https://github.com/stax-labs/stax2aws-releases/releases/download/v#{version}/stax2aws_#{version}_linux_amd64.tar.gz"
-    sha256 '92bffef3eca6cc9f61c88bb270f6051607a5dfdbc9ad75379814f7035979e634'
+    sha256 '3195c1d3b64081fcf1a6c8e826658e4342fac960bb04777d65d7f72fef5c37b0'
   end
 
   depends_on :arch => :x86_64
